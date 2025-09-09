46663089726d9dc1d214f9c24b34675ec57b1a3c
aryan <aryanuchia54@gmail.com>
2025-09-09 14:38:19 +0000

    Same error what should I do this won't fix easily i have got this error

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,7 +7,7 @@ const firebaseConfig = {
   projectId: "mindbloom-l8ow7",
   appId: "1:202583968080:web:67b025852515f4d07d8455",
   storageBucket: "mindbloom-l8ow7.firebasestorage.app",
-  apiKey: process.env.GEMINI_API_KEY || "AIzaSyAPZaKwIGQiHuiynQH47jx35k7rlZSanoM",
+  apiKey: "AIzaSyAPZaKwIGQiHuiynQH47jx35k7rlZSanoM",
   authDomain: "mindbloom-l8ow7.firebaseapp.com",
   measurementId: "",
   messagingSenderId: "202583968080"
