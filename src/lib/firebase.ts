8f268063bae4e986c8f9372f9b6ba9db5a07bde7
aryan <aryanuchia54@gmail.com>
2025-09-14 04:57:51 +0000

    In Next.js, only variables starting with NEXT_PUBLIC_ will be available

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,13 @@ import { getAuth, Auth } from "firebase/auth";
 import { getFirestore, Firestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAPZaKwIGQiHuiynQH47jx35k7rlZSanoM",
-  authDomain: "mindbloom-l8ow7.firebaseapp.com",
-  projectId: "mindbloom-l8ow7",
-  storageBucket: "mindbloom-l8ow7.firebasestorage.app",
-  messagingSenderId: "202583968080",
-  appId: "1:202583968080:web:67b025852515f4d07d8455",
-  measurementId: ""
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // This helper function ensures that we have a single instance of the Firebase app.
