afcca4b92767204915e11e730d14f6209fc965bc
aryan <aryanuchia54@gmail.com>
2025-09-14 08:24:07 +0000

    i want you to change the logo on the title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning style={{'--header-height': '64px'} as React.CSSProperties}>
-      <head />
+      <head>
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🌸</text></svg>" />
+      </head>
       <body className={cn('font-body antialiased', inter.variable)}>
         <ThemeProvider
           attribute="class"
