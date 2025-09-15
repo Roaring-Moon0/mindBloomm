da6a74c6c83dca9babd57afce36c69365793e736
aryan <aryanuchia54@gmail.com>
2025-09-15 12:19:19 +0000

    should i commint now and then try

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   output: 'standalone',
+  dynamic: 'force-dynamic',
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
