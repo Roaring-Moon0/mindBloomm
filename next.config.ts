1a12e3519754281d927371e6005caadfcb7d65ff
aryan <aryanuchia54@gmail.com>
2025-09-15 15:00:39 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   output: 'standalone',
-  dynamic: 'force-dynamic',
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
